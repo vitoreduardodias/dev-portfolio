7f2e75a3bb41ca0a41c08de4a50814250c809be6
vitoreduardodias <vit.edw@gmail.com>
2023-10-19 08:48:59 -0300

    Create Project and Class
    I was create the project and Suppliers class

---
+++ Razor/MyStock/Models/ErrorViewModel.cs
@@ -0,0 +1,9 @@
+namespace MyStock.Models
+{
+    public class ErrorViewModel
+    {
+        public string? RequestId { get; set; }
+
+        public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
+    }
+}
---
+++ Razor/MyStock/Models/Tables/Supplier.cs
@@ -0,0 +1,24 @@
+﻿using System.ComponentModel;
+using System.ComponentModel.DataAnnotations;
+
+namespace MyStock.Models.Tables
+{
+    public class Supplier
+    {
+        [Key]
+        [DisplayName("Id")]
+        public int Id { get; set; }
+
+        [Required(ErrorMessage = "Please provide a name")]
+        [MinLength(3, ErrorMessage = "The name should contain at least 3 characters")]
+        public string Name { get; set; } = string.Empty;
+
+        [Required(ErrorMessage = "Please provide a address")]
+        [MinLength(3, ErrorMessage = "The address should contain at least 3 characters")]
+        public string Address { get; set; } = string.Empty;
+
+        [Required(ErrorMessage = "Please provide a contact")]
+        [MinLength(3, ErrorMessage = "The contact should contain at least 3 characters")]
+        public string Contact { get; set; } = string.Empty;
+    }
+}
