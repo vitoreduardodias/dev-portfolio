2356aea1040667409fc32430047a0da038c0dec2
vitoreduardodias <vit.edw@gmail.com>
2023-10-23 22:38:28 -0300

    Fix the update bug (Entries)
    Create a Trigger "trgAfterUpdateEntry"

--- Razor/MyStock/Data/ApplicationDbContext.cs
+++ Razor/MyStock/Data/ApplicationDbContext.cs
@@ -19,6 +19,8 @@ namespace MyStock.Data
         {
             modelBuilder.Entity<Entry>()
                 .ToTable(tb => tb.HasTrigger("trgAfterInsertEntry"));
+            modelBuilder.Entity<Product>()
+                .ToTable(tb => tb.HasTrigger("trgAfterUpdateEntry"));
         }
     }
 }
