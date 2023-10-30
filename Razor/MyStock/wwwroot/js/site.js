815dced46496e92c1d751246ca24debe77ace0d1
vitoreduardodias <vit.edw@gmail.com>
2023-10-30 09:13:43 -0300

    Theme Implementation

--- Razor/MyStock/wwwroot/js/site.js
+++ Razor/MyStock/wwwroot/js/site.js
@@ -2,3 +2,11 @@
 // for details on configuring this project to bundle and minify static web assets.
 
 // Write your JavaScript code.
+
+// Write your JavaScript code.
+
+let table = new DataTable('#table-category');
+let table2 = new DataTable('#table-entry');
+let table3 = new DataTable('#table-supplier');
+let table4 = new DataTable('#table-output');
+let table5 = new DataTable('#table-product');
