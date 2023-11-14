179d5e9c5826c79c89c1185676ef7da5c7b91f59
vitoreduardodias <vit.edw@gmail.com>
2023-11-14 09:03:23 -0300

    Make the Header

--- FrontEnd/vitordias/src/App.vue
+++ FrontEnd/vitordias/src/App.vue
@@ -1,49 +1,42 @@
-<script setup>
-import HelloWorld from './components/HelloWorld.vue'
-import TheWelcome from './components/TheWelcome.vue'
-</script>
-
 <template>
-  <header>
-    <img alt="Vue logo" class="logo" src="./assets/logo.svg" width="125" height="125" />
-
-    <div class="wrapper">
-      <HelloWorld msg="You did it!" />
-    </div>
-
-    <button type="button" class="btn btn-primary">Primary</button>
-  </header>
-
-  <main>
-    <TheWelcome />
-  </main>
+  <div id="app">
+    <header>
+      <TheHeader />
+    </header>
+    <main>
+      <TheMain />
+    </main>
+    <footer>
+      <!-- Rodapé do aplicativo -->
+    </footer>
+  </div>
 </template>
 
+<script>
+
+import TheHeader from './components/TheHeader.vue';
+import TheMain from './components/TheMain.vue'
+
+export default {
+  name: 'App',
+  data() {
+    return {
+      // Dados do componente
+    };
+  },
+  methods: {
+    // Métodos do componente
+  },
+  computed: {
+    // Propriedades computadas
+  },
+  components: {
+    TheHeader,
+    TheMain
+  },
+};
+</script>
+
 <style scoped>
-header {
-  line-height: 1.5;
-}
-
-.logo {
-  display: block;
-  margin: 0 auto 2rem;
-}
-
-@media (min-width: 1024px) {
-  header {
-    display: flex;
-    place-items: center;
-    padding-right: calc(var(--section-gap) / 2);
-  }
-
-  .logo {
-    margin: 0 2rem 0 0;
-  }
-
-  header .wrapper {
-    display: flex;
-    place-items: flex-start;
-    flex-wrap: wrap;
-  }
-}
+/* Estilos do componente */
 </style>
---
+++ FrontEnd/vitordias/src/components/TheMain.vue
@@ -0,0 +1,28 @@
+<template>
+<h1>Hello Main!</h1>
+</template>
+  
+  <script>
+  export default {
+    name: 'TheMain',
+    props: {
+      // Propriedades do componente
+    },
+    data() {
+      return {
+        // Dados do componente
+      };
+    },
+    methods: {
+      // Métodos do componente
+    },
+    computed: {
+      // Propriedades computadas
+    },
+  };
+  </script>
+  
+  <style scoped>
+  
+  </style>
+  
