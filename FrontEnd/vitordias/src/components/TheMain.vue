1c5d87486ee31a38d6cf47613a8abc501066e225
vitoreduardodias <vit.edw@gmail.com>
2023-11-16 08:03:29 -0300

    NAV Corrections

--- FrontEnd/vitordias/src/components/TheMain.vue
+++ FrontEnd/vitordias/src/components/TheMain.vue
@@ -1,28 +1,38 @@
 <template>
-<h1>Hello Main!</h1>
+  <div class="">
+    <div class="row">
+      <div class="d-flex justify-content-center">
+        <h1 class="fw-bold">Your next BackEnd Developer</h1>
+    </div> 
+  </div>
+  <div class="d-flex flex-column">
+        <p>C#, .NET Core, SQL are Vitor' favorite tools, an 20-year-old programmer</p>
+        <p>who likes challenges and innovation. He creates smart, elegant solutions</p>
+        <p>to a variety of problems.</p>
+      </div>   
+  </div>
 </template>
   
-  <script>
-  export default {
-    name: 'TheMain',
-    props: {
-      // Propriedades do componente
-    },
-    data() {
-      return {
-        // Dados do componente
-      };
-    },
-    methods: {
-      // Métodos do componente
-    },
-    computed: {
-      // Propriedades computadas
-    },
-  };
-  </script>
+<script>
+export default {
+  name: 'TheMain',
+  props: {
+    // Propriedades do componente
+  },
+  data() {
+    return {
+      // Dados do componente
+    };
+  },
+  methods: {
+    // Métodos do componente
+  },
+  computed: {
+    // Propriedades computadas
+  },
+};
+</script>
   
-  <style scoped>
+<style scoped>
   
-  </style>
-  
+</style>
