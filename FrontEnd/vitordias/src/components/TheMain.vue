a2e76d37f01d1060e456f6f5de366806c4cf0b12
vitoreduardodias <vit.edw@gmail.com>
2023-11-16 10:29:30 -0300

    Doc the code

--- FrontEnd/vitordias/src/components/TheMain.vue
+++ FrontEnd/vitordias/src/components/TheMain.vue
@@ -1,15 +1,58 @@
 <template>
-  <div class="">
-    <div class="row">
-      <div class="d-flex justify-content-center">
-        <h1 class="fw-bold">Your next BackEnd Developer</h1>
+
+  <!-- Section 1 -->
+  <div class="container">
+    <div class="">
+      <!-- Text Area -->
+      <div class="text-center">
+        <!-- Title -->
+        <div>
+        <h1 class="fw-bold fs-1">Your next BackEnd Developer</h1>
+      </div>
+
+      <!-- Description -->
+      <div>
+        <p class="lh-sm fs-5">C#, .NET Core, SQL are Vitor' favorite tools, an 20-year-old programmer, 
+          <br> who likes challenges and innovation. He creates smart, elegant solutions 
+          <br> to a variety of problems.</p>
+        </div>
+
+        <!-- Buttons -->
+        <div class="">
+          <button type="button" class="btn btn-dark"><i class="bi bi-github"></i> Github</button>
+          <button type="button" class="btn btn-dark"><i class="bi bi-linkedin"></i> LinkedIn</button>
+        </div>
+
     </div> 
   </div>
-  <div class="d-flex flex-column">
-        <p>C#, .NET Core, SQL are Vitor' favorite tools, an 20-year-old programmer</p>
-        <p>who likes challenges and innovation. He creates smart, elegant solutions</p>
-        <p>to a variety of problems.</p>
-      </div>   
+
+<!-- Section 2 -->
+  
+  <div class="row row-cols-2">
+  <div>
+    <!-- Title -->
+      <div>
+        <h1 class="fw-bold fs-1">About</h1>
+      </div>
+      
+      <!-- Description -->
+      <div>
+        <p class="lh-sm fs-6">Lorem Ipsum is simply dummy text of the printing and typesetting industry. Lorem Ipsum has been the industry's standard dummy text ever since the 1500s, when an unknown printer took a galley of type and scrambled it to make a type specimen book. It has survived not only five centuries, but also the leap into electronic typesetting, remaining essentially unchanged.</p>
+      </div>
+  </div>
+
+  <!-- Skills -->
+  <div>
+    <button type="button" class="btn btn-dark"><i class="bi bi-github"></i> Github</button>
+    <button type="button" class="btn btn-dark"><i class="bi bi-linkedin"></i> LinkedIn</button>
+    <button type="button" class="btn btn-dark"><i class="bi bi-github"></i> Github</button>
+    <button type="button" class="btn btn-dark"><i class="bi bi-linkedin"></i> LinkedIn</button>
+    <button type="button" class="btn btn-dark"><i class="bi bi-github"></i> Github</button>
+    <button type="button" class="btn btn-dark"><i class="bi bi-linkedin"></i> LinkedIn</button>
+    <button type="button" class="btn btn-dark"><i class="bi bi-github"></i> Github</button>
+    <button type="button" class="btn btn-dark"><i class="bi bi-linkedin"></i> LinkedIn</button>
+  </div>
+</div>
   </div>
 </template>
   
