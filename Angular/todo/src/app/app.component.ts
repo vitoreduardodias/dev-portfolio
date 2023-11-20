8e419f312973821b36218b0383c32673097359e5
vitoreduardodias <vit.edw@gmail.com>
2023-11-20 09:09:30 -0300

    Complex Objects

--- Angular/todo/src/app/app.component.ts
+++ Angular/todo/src/app/app.component.ts
@@ -1,14 +1,25 @@
 import { Component } from '@angular/core';
 import { CommonModule } from '@angular/common';
 import { RouterOutlet } from '@angular/router';
+import { Todo } from '../models/todo.model';
 
 @Component({
   selector: 'app-root', // <app-root>
   standalone: true,
-  imports: [CommonModule, RouterOutlet],
+  imports: [CommonModule],
   templateUrl: './app.component.html',
   styleUrl: './app.component.css'
 })
 export class AppComponent {
-  title = 'todo';
+  title = 'My Tasks';
+  public todos: Todo[] = [];
+
+
+  constructor() {
+    this.todos.push(new Todo(1, 'Walk with dog', false));
+    this.todos.push(new Todo(2 , 'Cut the hair', true));
+    this.todos.push(new Todo(3, 'Go to market', false));
+    this.todos.push(new Todo(4, "sdadads", false))
+    
+  }
 }
