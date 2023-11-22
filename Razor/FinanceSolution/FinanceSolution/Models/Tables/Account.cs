965d946434121327a2ec0671a4e09560497b2f96
vitoreduardodias <vit.edw@gmail.com>
2023-11-22 10:55:51 -0300

    Update Account.cs

--- Razor/FinanceSolution/FinanceSolution/Models/Tables/Account.cs
+++ Razor/FinanceSolution/FinanceSolution/Models/Tables/Account.cs
@@ -1,6 +1,22 @@
-﻿namespace FinanceSolution.Models.Tables;
+﻿using System.ComponentModel;
+using System.ComponentModel.DataAnnotations;
+namespace FinanceSolution.Models.Tables;
 
 public class Account
 {
-    
+    [Key]
+    [DisplayName("Id")]
+    public int Id { get; set; }
+
+    [Required(ErrorMessage = "Please provide a name")]
+    [MinLength(3, ErrorMessage = "The name should contain at least 3 characters")]
+    [StringLength(50, ErrorMessage = "The name should contain up to 50 characters")]
+    [DisplayName("Name")]
+    public string Name { get; set; } = string.Empty;
+
+
+
+    public List<Release> Releases { get; set; } = new();
+
+        
 }
